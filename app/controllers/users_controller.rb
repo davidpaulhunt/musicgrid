e92b7bd76e6b7c459a74ecdd3a419dd6a9be8ae8
txdavidtx <davidpaulhunt@gmail.com>
2014-06-07 14:46:58 -0400

    added logging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email)
-    # (:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
   end
 
   def set_user
