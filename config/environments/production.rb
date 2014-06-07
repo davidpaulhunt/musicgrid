8868387da5eee56106b98d431ed086c5f00db3ce
txdavidtx <davidpaulhunt@gmail.com>
2014-06-07 14:22:53 -0400

    added logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.log_tags = [ :subdomain, :uuid ]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -43,10 +43,10 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
